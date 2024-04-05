f7b10ea87d3545902af4ebd9b734d7e6e8e9bea6
Lilya01 <Lilya_Kireeva01@mail.ru>
2024-04-05 22:29:06 +0300

    for conflict

--- dfs.py
+++ dfs.py
@@ -1,22 +1,26 @@
+#добавил коментарии
 def dfs(graph, start, end, visited=None, path=None):
+    #qwewqewqeqweqwe
     if visited is None:
         visited = set()
     if path is None:
         path = []
-
+    #дабвляение в посещение
     visited.add(start)
+    #добавление в путь
     path.append(start)
 
     if start == end:
         return path, len(path) - 1
-
+    #основной алгоритм
     for neighbor in graph.get(start, []):
         if neighbor not in visited:
             result_path, path_length = dfs(graph, neighbor, end, visited, path)
             if result_path:
                 return result_path, path_length
-
+    #путь
     path.pop()
+    #удаление
     visited.remove(start)
 
     return None, 0
@@ -33,11 +37,14 @@ graph = {
 vertex_a = 2
 vertex_b = 4
 
-# Получение пути обхода и длины пути от вершины a до вершины b
-result_path, path_length = dfs(graph, vertex_a, vertex_b)
-
-if result_path:
-    print("Путь:", result_path)
-    print("Длина пути от вершины {} до вершины {}: {}".format(vertex_a, vertex_b, path_length))
+if vertex_a not in graph or vertex_b not in graph:
+    print("Вершины {} или {} отсутствуют в графе.".format(vertex_a, vertex_b))
 else:
-    print("Путь от вершины {} до вершины {} не найден.".format(vertex_a, vertex_b))
+    # Получение пути обхода и длины пути от вершины a до вершины b
+    result_path, path_length = dfs(graph, vertex_a, vertex_b)
+
+    if result_path:
+        print("Путь:", result_path)
+        print("Длина пути от вершины {} до вершины {}: {}".format(vertex_a, vertex_b, path_length))
+    else:
+        print("Путь от вершины {} до вершины {} не найден.".format(vertex_a, vertex_b))
